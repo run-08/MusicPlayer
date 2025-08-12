1f1729337d541cfe3b51e03166049ad3dd6e8df0
Arun Thangavel <aru701567@gmail.com>
2025-08-12 20:27:05 +0530

    Enhanced UI for Singers Playlist

--- src/ArtistBasedSong/ArtistsSongs.jsx
+++ src/ArtistBasedSong/ArtistsSongs.jsx
@@ -1,23 +1,82 @@
-import { useState } from "react";
-
+import ColorThief from "colorthief";
+import { useEffect, useRef, useState } from "react";
 const ArtistSongs = () => {
+  const [bgColor, setBgColor] = useState("black");
+  const imgRef = useRef(null);
+  useEffect(() => {
+    const img = imgRef.current;
+    if (!img) return;
+    img.crossOrigin = "anonymous";
+    img.addEventListener("load", () => {
+      const colorThief = new ColorThief();
+      const colorPallete = colorThief.getPalette(img, 3);
+      console.log(colorPallete);
+      const colors = colorPallete
+        .map((c) => `rgb(${c[0]},${c[1]},${c[2]})`)
+        .sort(() => Math.random() - 0.5);
+      const angle = Math.floor(Math.random() * 360);
+      const gradient = `linear-gradient(${angle}deg , ${colors.join(",")})`;
+      setBgColor(gradient);
+      console.log(gradient);
+    });
+  }, []);
+  const ArtistsSongs = [
+    {
+      title: "Monica",
+      Duration: "5:00",
+      Author: "Aniruth Ravichandran",
+    },
+    {
+      title: "Jawan",
+      Duration: "4:00",
+      Author: "Aniruth Ravichandran",
+    },
+    {
+      title: "Hukum Thalaivar",
+      Duration: "3:00",
+      Author: "Aniruth Ravichandran",
+    },
+    {
+      title: "Sirrikathey",
+      Duration: "4:46",
+      Author: "Aniruth Ravichandran",
+    },
+    {
+      title: "VIP",
+      Duration: "3:00",
+      Author: "4:00",
+    },
+  ];
   const ArtistsFameSongs = useState([]);
   return (
     <>
-      <div className="ArtistsSong p-0">
-        <div className="artists-image  my-10 flex justify-center items-center">
+      <div
+        className={`ArtistsSong p-0 h-120 `}
+        style={{
+          background: bgColor,
+        }}
+      >
+        <div className="artists-image mr-40 flex justify-center items-center">
           <img
             src={
-              "https://www.deccanchronicle.com/h-upload/2025/03/19/1900060-untitleddesign84.webp"
+              "https://variety.com/wp-content/uploads/2024/03/AR-Rahman-Firdaus.jpg?w=1000&h=563&crop=1"
             }
-            className="object-cover w-120 h-120 mr-300 border box-border cursor-pointer p-0 m-0 "
-            alt=""
+            className="object-cover w-90 h-90 my-10 mr-00 border rounded-xl box-border cursor-pointer p-0 m-0 "
+            alt="Aniruth Ravichandran"
+            onClick={() => alert()}
+            crossOrigin="anonymous"
+            ref={imgRef}
           ></img>
         </div>
-        <div className="artists_songs my-10 absolute mx-4 w-[calc(100%-2rem)] bg-black h-120">
+        <div className="artists_songs my-13 ring-4 bg-[linear-gradient(to_bottom,black_30%,#828282_100%)] absolute mx-2 w-[calc(100%-1rem)] rounded-md h-120">
           <div className="playlist ">
-            <div className="song-bar  mx-10 h-15 bg-white  cursor-pointer mt-10 ring-3 hover:ring-purple-300 rounded-xl"></div>
-            <div className="song-bar  mx-10 h-15 bg-white  cursor-pointer mt-10 ring-3 hover:ring-purple-300 rounded-xl"></div> 
+            {ArtistsSongs &&
+              ArtistsSongs.map((items, key) => (
+                <div
+                  key={key}
+                  className="song-bar  mx-10 h-14    bg-white transition transform ease-in-out  hover:128 cursor-pointer mt-10 ring-3 hover:ring-purple-300 rounded-md"
+                ></div>
+              ))}
           </div>
         </div>
       </div>
